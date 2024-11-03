785e6d86c74eacf0c36fcb7fbc98a22addd857f8
ktoregan <ktdevmail@proton.me>
2024-11-03 14:37:26 +0000

    hook & code fixes

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'TDBot',
+        script: 'bot.js',
+      },
+      {
+        name: 'TDBotWebhook',
+        script: 'webhookServer/webhook.js',
+      },
+    ],
+  };  
